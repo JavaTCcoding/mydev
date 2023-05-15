0008b4213f4e82f04e31e4d7823c18168b18a912
JavaTCcoding <133637103+JavaTCcoding@users.noreply.github.com>
2023-05-15 17:03:58 +0530

    This a calculator programming

---
+++ calculator.java
@@ -0,0 +1,10 @@
+public class calculator{
+public static void main(String[] args){
+int x=10;
+int y=20;
+System.out.println("addition"+(x+y));
+System.out.println("sub"+(x-y));
+System.out.println("mul"+(x*y));
+System.out.println("div"+(y/x));
+}
+}
